e4484e14ea87f889ed70cd11f0f2864eac5cd7f4
austinbrovick <austinbrovick@gmail.com>
2016-12-13 13:20:42 -0800

    commit after finishing

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,13 +10,20 @@ class SearchBar extends Component { // define a new class called SearchBar and g
 
 	render() {
 		return (
-			<div>
+			<div className="search-bar">
 				<input 
 					value={this.state.term}
-					onChange={event => this.setState({term: event.target.value})} />
+					onChange={event => this.onInputChange(event.target.value)} />
 			</div>
 		);
 	}
+
+
+	onInputChange(term) {
+		this.setState({term}); 
+		this.props.onSearchTermChange(term); 
+	}
+
 
 }
 
